49b6d74dda227126d953085692807fe870a67345
maruhachi <konchan1129@gmail.com>
2018-07-01 19:43:11 +0900

    add setting for H2 datasource

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT into user(id, name)
+ values
+(1, 'honoka'),
+(2, 'kotori'),
+(3, 'umi');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE if not exists user
+(
+    id int PRIMARY KEY,
+    name varchar(32)
+);
